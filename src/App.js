d08893cf663b905e7377f5da611c7bbd20efff06
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-11-04 16:56:12 -0600

    added productcontext and imported to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from './data';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
+import { ProductContext } from 
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
