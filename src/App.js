2331eef0730dc387d05ac525b9866741f9b0d2c2
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-11-04 17:32:35 -0600

    removed props from navigation and shopping cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
-import { ProductContext } from 
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
@@ -19,24 +20,26 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
-				{/* // 	exact
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
+					{/* // 	exact
 				// 	path="/"
 				// 	render={() => ( */}
-				{/* // 		<Products
+					{/* // 		<Products
 				// 			products={products}
 				// 			addItem={addItem}
 				// 		/>
 				// 	)}
 				// /> */}
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
